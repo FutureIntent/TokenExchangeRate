e370747ebf9c5b6ed0324ae1041dbe0bedd00e75
nikitak <nikitak@shedevre.com>
2024-05-07 17:08:05 +0300

    small fix

--- tokenExchangeRate.js
+++ tokenExchangeRate.js
@@ -5,6 +5,8 @@ var V3FactoryAbi = JSON.parse(fs.readFileSync('./abi/V3Factory.json'));
 var V3PoolAbi = JSON.parse(fs.readFileSync('./abi/V3Pool.json'));
 
 var web3 = new Web3("https://mainnet.infura.io/v3/be89ea96062a447cb9a2500c4fff8143");
+
+// 10000 - 1%
 var fee = 10000;
 
 const uniswapV3FactoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
@@ -46,7 +48,7 @@ const getExchangeRate = async ({ token0, token1, amount }) => {
     }
 
     const exchangeRate = parceSqrtPriceX96ToDecimal(sqrtPriceX96, token1Decimals, token0Decimals);
-    const exchangeAmount = 1 / exchangeRate;
+    const exchangeAmount = amount * 1 / exchangeRate;
 
     return {
         [token0]: amount,
@@ -57,9 +59,9 @@ const getExchangeRate = async ({ token0, token1, amount }) => {
 const initialise = async () => {
 
     const exchangeRateObject = {
-        token0: 'TONCOIN',
-        token1: 'WETH',
-        amount: 1 // token0 amount
+        token0: 'WETH',
+        token1: 'SHIB',
+        amount: 1.5 // token0 amount
     };
 
     const result = await getExchangeRate(exchangeRateObject);
